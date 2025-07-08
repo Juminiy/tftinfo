323cbf0fe3ade9569184f3085a0823bf7b5d0c0d
Chisato <hln0x29a@gmail.com>
2025-07-08 13:28:25 +0800

    first init

---
+++ traits/process.py
@@ -0,0 +1,44 @@
+# value like that
+
+# first we need parse each file into a obj json-that:
+# {'s1': {"profession": [], "race": []}}
+
+import os
+from typing import Any,Dict
+
+
+def process_dict() -> Dict[str,Any]:
+    """
+        return {
+            's_': {
+                'name': '',
+                'profession': [],
+                'race': [],
+                '5': []
+            }
+        }
+    """
+    sdict=dict[str,Any]()
+    for i in range(1,15):
+        with open(f's{i}.txt', 'r', encoding='utf-8') as sfile:
+            wds=[line.strip() for line in sfile.readlines()]
+            sdict[f's{i}'] = {}
+            sdict[f's{i}']['name'] = wds[0].removeprefix('# ')
+            wds=wds[1:]
+            wdi=0
+            while wdi < len(wds):
+                while wdi < len(wds) and len(wds[wdi]) == 0:
+                    wdi+=1
+                starti=wdi
+                while wdi < len(wds) and len(wds[wdi]) > 0:
+                    wdi+=1
+                cate=wds[starti].removeprefix('# ').lower()
+                sdict[f's{i}'][cate] = wds[starti+1:wdi]
+    
+    return sdict
+
+from json import dumps,loads
+s_star_json=dumps(process_dict(), indent='  ', ensure_ascii=False)
+with open('s_all.json', 'w+') as sstarf:
+    sstarf.write(s_star_json)
+    sstarf.close()
---
+++ traits_v2/process.py
@@ -0,0 +1,118 @@
+from typing import (List,Mapping,Optional,Dict,Any,Tuple)
+
+import os 
+
+def next_chuck(lines: List[str], cursor: int) -> Tuple[int,int,bool]:
+    """
+        return Tuple[int,int,int] (chunk_si, chunk_ei, valid)
+    """
+    while 0<=cursor < len(lines):
+        while cursor < len(lines) and len(lines[cursor]) == 0:
+            cursor+=1
+        starti = cursor
+        while cursor < len(lines) and len(lines[cursor]) > 0:
+            cursor+=1
+        if cursor > starti:
+            return (starti, cursor, True)
+        else:
+            return (-1,-1,False)
+    return (-1,-1,False)
+
+def get_origins_classes(l3: List[str]) -> List[dict[str,Any]]:
+    res:List[dict[str,Any]]=[]
+    if len(l3) == 0:
+        return res
+    l3key=[item.strip().strip('{').strip('}') for item in l3[0].split('|')]
+    for ls in l3[1:]:
+        l3item=[item.strip() for item in ls.split('|')]
+        l3dict={}
+        for idx in range(len(l3item)):
+            l3dict[l3key[idx]] = l3item[idx]
+        res.append(l3dict)
+    return res
+
+def get_grid_dict(lgrid: List[str]) -> Dict[str,Any]:
+    res:dict[str,Any]={
+        'grid': [],
+        'maps': {
+            'origins': {},
+            'classes': {}
+        }
+    }
+    if len(lgrid) == 0:
+        return res
+    d2grid=[ls.split('|') for ls in lgrid]
+    m,n=len(d2grid),len(d2grid[0])
+    for i in range(m):
+        for j in range(n):
+            d2grid[i][j]=d2grid[i][j].strip()
+    res['grid'] = d2grid
+    
+    def split_unit(unit_i: str) -> List[str]:
+        return [unit_each for unit_each in unit_i.split('/') if len(unit_each) > 0]
+    originsRes=dict[str,List[str]]()
+    classesRes=dict[str,List[str]]()
+    for i in range(1, m):
+        originsRes[d2grid[i][0]] = []
+        for j in range(1, n):
+            if len(d2grid[i][j]) > 0:
+                if d2grid[i][0] not in originsRes:
+                    originsRes[d2grid[i][0]] = []
+                originsRes[d2grid[i][0]].extend(split_unit(d2grid[i][j]))
+        originsRes[d2grid[i][0]] = list(set(originsRes[d2grid[i][0]]))
+    for j in range(1,n):
+        classesRes[d2grid[0][j]]= []
+        for i in range(1,m):
+            if len(d2grid[i][j]) > 0:
+                if d2grid[0][j] not in classesRes:
+                    classesRes[d2grid[0][j]] = []
+                classesRes[d2grid[0][j]].extend(split_unit(d2grid[i][j]))
+        originsRes[d2grid[0][j]] = list(set(classesRes[d2grid[0][j]]))
+    res['maps']['origins'] = originsRes
+    res['maps']['classes'] = classesRes
+    return res
+
+def process_tft() -> Dict[str,Any]:
+    sdict:dict[str,Any]={}
+    for seti in range(1,15):
+        setof=f's{seti}'
+        setfilename=f'{setof}.txt'
+        if not os.path.exists(setfilename) or \
+            not os.path.isfile(setfilename):
+            continue
+        with open(setfilename,'r',encoding='utf-8') as setfile:
+            lines=[line.strip() for line in setfile.readlines()]
+            sdict[setof] = {}
+            sdict[setof]['name'] = lines[0].removeprefix('# ')
+            lines=lines[1:]
+            linei=0
+            curl2=''
+            while 0 <= linei < len(lines):
+                (starti, linei, ok) = next_chuck(lines, linei)
+                if ok:
+                    if starti < len(lines) and lines[starti].startswith('## '):
+                        curl2=lines[starti].removeprefix('## ').lower()
+                        sdict[setof][curl2] = {}
+                        starti+=1
+                    if starti < len(lines) and lines[starti].startswith('### '):
+                        curl3=lines[starti].removeprefix('### ').lower()
+                        if curl3 in ['origins', 'classes']:
+                            sdict[setof][curl2][curl3] = \
+                                get_origins_classes(lines[starti+1:linei])
+                        elif curl3 == 'synergygrid':
+                            sdict[setof][curl2][curl3] = \
+                                get_grid_dict(lines[starti+1:linei])
+                        else:
+                            sdict[setof][curl2][curl3] = lines[starti+1:linei]
+
+    return sdict
+
+from json import dumps, loads
+tftinfo=dumps(
+    process_tft(),
+    ensure_ascii=True,
+    indent='    ',
+)
+with open('s_all.json', 'w+') as tftfile:
+    tftfile.write(tftinfo)
+    tftfile.close()
